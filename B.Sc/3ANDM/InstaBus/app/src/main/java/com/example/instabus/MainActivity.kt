f6112033bb8e6602f9d2cc499edd1c19d988b5e4
Paul Riviere <paul.riviere@supinfo.com>
2020-02-26 09:56:29 +0100

    liste arrets en dur et scrollable

--- B.Sc/3ANDM/InstaBus/app/src/main/java/com/example/instabus/MainActivity.kt
+++ B.Sc/3ANDM/InstaBus/app/src/main/java/com/example/instabus/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.instabus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.instabus.fragments.BusStopListFragment
 import com.example.instabus.fragments.BusStopMapFragment
 import com.example.instabus.fragments.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var liste = arrayOf("a", "b", "c", "d", "e", "f", "g", "h")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +24,23 @@ class MainActivity : AppCompatActivity() {
         adapter.addFragment(BusStopMapFragment(), getString(R.string.tab_bus_stop_map))
         viewPager.adapter = adapter
         tabs.setupWithViewPager(viewPager)
+
+
     }
+
+//    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        //alimentation liste d'arrets dans le fragment
+//        val linearLayoutStops = findViewById<LinearLayout>(R.id.busStopListLayout)
+//        for (stop in liste){
+//            val textView =  TextView(this)
+//            textView.text = stop
+//            textView.layoutParams = LinearLayout.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT,
+//                1.0f
+//            )
+//            textView.textSize = 30F
+//            linearLayoutStops.addView(textView)
+//        }
+//    }
 }
--- B.Sc/3ANDM/InstaBus/app/src/main/java/com/example/instabus/fragments/BusStopListFragment.kt
+++ B.Sc/3ANDM/InstaBus/app/src/main/java/com/example/instabus/fragments/BusStopListFragment.kt
@@ -1,13 +1,51 @@
 package com.example.instabus.fragments
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.instabus.R
+import java.net.URL
 
 class BusStopListFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        inflater.inflate(R.layout.fragment_bus_stop_list, container, false)!!
+    private var liste = arrayOf("station 1", "station 2", "station 3", "station 4", "station 5", "station 6", "station 7", "station 8",
+        "station 9", "station 10", "station 11", "station 12", "station 13", "station 14", "station 15", "station 16",
+        "station 17", "station 18", "station 19", "station 20", "station 21", "station 22", "station 23", "station 24",
+        "station 25", "station 26", "station 27", "station 28", "station 29", "station 30", "station 31", "station 32")
+
+    //private var listebis = URL("http://barcelonaapi.marcpous.com/bus/nearstation/latlon/41.3985182/2.1917991/1.json").readText()
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var layoutBusList = inflater.inflate(R.layout.fragment_bus_stop_list, container, false)
+        return layoutBusList
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //alimentation liste d'arrets dans le fragment
+        val linearLayoutStops = view?.findViewById<LinearLayout>(R.id.busStopListLayout)
+        for (stop in liste){
+            val textView =  TextView(activity)
+            textView.text = stop
+//            textView.layoutParams = LinearLayout.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT,
+//                1.0f
+//            )
+            textView.textSize = 30F
+            textView.gravity = Gravity.LEFT
+//            textView.setClickable(true)
+            linearLayoutStops?.addView(textView)
+        }
+    }
 }
