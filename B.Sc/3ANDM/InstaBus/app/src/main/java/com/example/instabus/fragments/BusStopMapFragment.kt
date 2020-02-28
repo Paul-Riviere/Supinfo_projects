14d5c9c86ae11354abe6eb52e3ce55792f9f68b3
Paul Riviere <paul.riviere@supinfo.com>
2020-02-28 01:54:38 +0100

    map instanciée sans signets

--- B.Sc/3ANDM/InstaBus/app/src/main/java/com/example/instabus/fragments/BusStopMapFragment.kt
+++ B.Sc/3ANDM/InstaBus/app/src/main/java/com/example/instabus/fragments/BusStopMapFragment.kt
@@ -5,16 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.instabus.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class BusStopMapFragment : Fragment() {
+class BusStopMapFragment : Fragment(), OnMapReadyCallback {
+
+    private var googleMap: GoogleMap?=null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var layoutBusList = inflater.inflate(R.layout.fragment_bus_stop_map, container, false)
-        return layoutBusList
+
+        var layoutBusMap = inflater.inflate(R.layout.fragment_bus_stop_map, container, false)
+        return layoutBusMap
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val mapFragment = childFragmentManager?.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        googleMap=p0
+
+        //Adding markers to map
+
+        val latLng= LatLng(41.3985182,2.1917991)
+        val markerOptions: MarkerOptions =MarkerOptions().position(latLng).title("New Delhi")
+
+        // moving camera and zoom map
+
+        val zoomLevel = 12.0f //This goes up to 21
+
+
+        googleMap.let {
+            it!!.addMarker(markerOptions)
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel))
+        }
     }
 }
